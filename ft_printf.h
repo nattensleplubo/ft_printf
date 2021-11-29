26917c7a99a66a5c9efe19df901532da18000499
Nathan GOBERT <ngobert@e1r9p15.42.fr>
2021-11-29 12:39:27 +0100

    Split in files

--- ft_printf.h
+++ ft_printf.h
@@ -6,19 +6,24 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-// int		ft_putchar(char c);
-// int		ft_putstr(char *str);
-// void	ft_putnbr(int n);
-// int		checkbase(char *str);
-// void	ft_putnbr_base(int nb, char *base);
-// void	ft_putunsigned(unsigned int n);
-// int		ft_print_char(va_list formatParams);
-// int		ft_print_str(va_list formatParams);
-// void	ft_print_decimal(va_list formatParams);
-// void	ft_putnbr_hex(va_list formatParams, char *base);
-// void	ft_print_unsigned(va_list formatParams);
-// int		ft_parse(va_list formatParams, int i, const char *format);
+int		ft_len(int n);
+int		ft_len_unsigned(unsigned int n);
+int		ft_hex_len(unsigned	int num);
+int		ft_ptr_len(unsigned long num);
+int		ft_putchar(char c);
+int		ft_putstr(char *str);
+void	ft_putnbr(int n);
+void	ft_putnbr_base(int nb, char *base);
+void	ft_putunsigned(unsigned int n);
+void	ft_putpointer(unsigned long nb, char *base);
+int		ft_print_char(va_list formatParams);
+int		ft_print_str(va_list formatParams);
+int		ft_print_decimal(va_list formatParams);
+int		ft_putnbr_hex(va_list formatParams, char *base);
+int		ft_print_unsigned(va_list formatParams);
+int		ft_print_pointer(va_list formatParams);
+int		ft_parse(va_list formatParams, int i, const char *format);
 
-// int		ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
 
 #endif
